2c0f9ecf70f2c89969204de76c690e568ab2e37b
srabanimitra <srabanimitra2222@gmail.com>
2025-03-09 10:38:05 +0600

    Update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,29 +12,26 @@ import AdminCircular from "./views/AdminCircular";
 import Profile from "./views/Profile";
 import Welcome from "./views/Welcome";
 import Guideline from "./views/Guideline";
-import AdminHome from "./views/AdminHome"; // Import AdminHome
-import Sidebar from "./components/Sidebar"; // Import Sidebar
+import AdminHome from "./views/AdminHome";
+import Sidebar from "./components/Sidebar";
 import "./components/Header";
-import ManageCircular from "./views/ManageCircular"; // Import ManageCircular page
-import EditJobForm from "./views/EditJobForm"; // Import EditJobForm page
+import ManageCircular from "./views/ManageCircular";
+import EditJobForm from "./views/EditJobForm";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import ProfilePage from "./views/ProfilePage";
-import ViewDetails from "./views/Viewdetails";
-import Research from './views/Research';
+import ViewDetails from "./views/ViewDetails"; // Fixed import
+import Research from "./views/Research";
 import Apply from "./views/Apply";
-import UserManagement from './views/UserManagement';
+import UserManagement from "./views/UserManagement";
 import ChangePass from "./views/Changepass";
 import ApplicationManagement from "./views/ApplicationManagement";
-import AdvocacyPage from "./views/AdvocacyPage"; // Import Advocacy Page
+import AdvocacyPage from "./views/AdvocacyPage";
 import InnovationPage from "./views/InnovationPage";
-
 
 const clientId = "854801450505-s8587henb9cl084he14ap11pubok812o.apps.googleusercontent.com";
 
 const AppLayout = () => {
   const location = useLocation();
-
-  // Define admin-related routes where the footer should not be displayed
   const noFooterRoutes = ["/adminHome", "/sidebar", "/manage-jobs", "/edit-job/:id"];
 
   return (
@@ -54,21 +51,19 @@ const AppLayout = () => {
           <Route path="/adminHome" element={<AdminHome />} />
           <Route path="/sidebar" element={<Sidebar />} />
           <Route path="/adminCirculars" element={<AdminCircular />} />
-          <Route path="/manage-jobs" element={<ManageCircular />} />  
-          <Route path="/edit-job/:id" element={<EditJobForm />} /> 
-          <Route path="/profile-page" element={<ProfilePage />} /> 
+          <Route path="/manage-jobs" element={<ManageCircular />} />
+          <Route path="/edit-job/:id" element={<EditJobForm />} />
+          <Route path="/profile-page" element={<ProfilePage />} />
           <Route path="/jobs/:id" element={<ViewDetails />} />
           <Route path="/userManagement" element={<UserManagement />} />
           <Route path="/research" element={<Research />} />
           <Route path="/apply" element={<Apply />} />
           <Route path="/applicationManagement" element={<ApplicationManagement />} />
           <Route path="/change" element={<ChangePass />} />
-          <Route path="/advocacy" element={<AdvocacyPage />} /> {/* Advocacy Page */}
+          <Route path="/advocacy" element={<AdvocacyPage />} />
           <Route path="/innovation" element={<InnovationPage />} />
         </Routes>
       </div>
-
-      {/* Hide Footer for Admin Pages */}
       {!noFooterRoutes.includes(location.pathname) && <Footer />}
     </div>
   );
